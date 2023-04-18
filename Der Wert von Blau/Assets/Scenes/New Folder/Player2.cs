4de0a0b98ed5c7d240688ac558ee333cb3eccf8e
ERICK RAPHAEL GONZAGA FERREIRA <20211170150021@ifrn.local>
2023-04-18 14:33:51 -0300

    Renanzao

---
+++ Der Wert von Blau/Assets/Scenes/New Folder/Player2.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour
+{
+
+    public int velocidade;
+    public int forcaPulo;
+    public bool noAr;
+    public SpriteRenderer visual;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        noAr = false;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        noAr = true;
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+
+        Vector3 inputTeclado = new Vector3(x, 0, 0);
+        transform.position += inputTeclado * velocidade * Time.deltaTime;
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && !noAr)
+        {
+            Debug.Log("PRESSIONOU");
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * forcaPulo, ForceMode2D.Impulse);
+        }
+        
+        if(x < 0)
+        {
+            //olhando pra esquerda
+            visual.flipX = true;
+        }
+        else if(x > 0)
+        {
+            //olhando pra direita
+            visual.flipX = false;
+        }
+
+
+
+    }
+}
